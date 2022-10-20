e93671b8883ab515af847dc16e80dae1f6cb5ddf
MB7232SP <mauryaarvind962@gmail.com>
2022-10-21 01:53:06 +0530

    sdhj

--- configs/db.js
+++ configs/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connection = async () => {
   try {
-    console.log("databaseconnected");
+    // console.log("databaseconnected");
     mongoose.connect("mongodb+srv://arvind_maurya:arvind_194@cluster0.qxv1nzm.mongodb.net/?retryWrites=true&w=majority");
   } catch (error) {
     console.log(error);
