14ba64dabe172ba52921d67c4ce3580d9e5468c3
MB7232SP <mauryaarvind962@gmail.com>
2022-10-21 00:53:41 +0530

    backend

---
+++ configs/db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const connection = async () => {
+  try {
+    console.log("databaseconnected");
+    mongoose.connect("mongodb+srv://arvind_maurya:arvind_194@cluster0.qxv1nzm.mongodb.net/?retryWrites=true&w=majority");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// const connection =
+
+module.exports = connection;
